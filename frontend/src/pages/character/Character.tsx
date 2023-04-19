a70a9c8ba3907cf78dec40d040d97ebf807e5dff
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2023-04-19 21:42:45 +0200

    Add Helmet

--- frontend/src/pages/character/Character.tsx
+++ frontend/src/pages/character/Character.tsx
@@ -1,6 +1,11 @@
+import { Helmet } from 'react-helmet';
+
 export const Character = () => {
   return (
     <div className="character" data-testid="character">
+      <Helmet>
+        <title>Character</title>
+      </Helmet>
       character
     </div>
   );
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,6 +1,11 @@
+import { Helmet } from 'react-helmet';
+
 export const Home = () => {
   return (
     <div className="home" data-testid="home">
+      <Helmet>
+        <title>Primotly-SPA</title>
+      </Helmet>
       home
     </div>
   );
